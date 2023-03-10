6a1c17f4f0996ee4027e86b937dd0b6789fbb2fa
CHAEWONSEOK98 <7dugod7@naver.com>
2023-03-11 04:32:05 +0900

    delete-page,form-validation

--- src/pages/MainPages.jsx
+++ src/pages/MainPages.jsx
@@ -8,6 +8,8 @@ const MainPages = () => {
   const [mainPages, setMainPages] = useRecoilState(mainPagesState);
   const [pageName, setPageName] = useState('');
 
+  const [pageDeleteName, setPageDeleteName] = useState('');
+
   const [linkConnect, setLinkConnect] = useRecoilState(mainPagesConnect);
   const [linkName, setLinkName] = useState('');
   const [url, setUrl] = useState('');
@@ -15,6 +17,7 @@ const MainPages = () => {
   // page
   const handlepageSubmit = (e) => {
     e.preventDefault();
+    if (pageName.length === 0) return;
     setMainPages((prev) => [
       ...prev,
       {
@@ -29,9 +32,26 @@ const MainPages = () => {
     setPageName(e.target.value);
   };
 
+  // pageDelete
+  const handlePageDeleteSubmit = (e) => {
+    e.preventDefault();
+    if (pageDeleteName.length === 0) return;
+    setMainPages((prev) =>
+      prev.filter((element) => element.pageName !== pageDeleteName)
+    );
+    setPageDeleteName('');
+  };
+
+  const handlePageDelete = () => {};
+
+  const handlePageDeleteChange = (e) => {
+    setPageDeleteName(e.target.value);
+  };
+
   // link
   const handleCreateLinkSubmit = (e) => {
     e.preventDefault();
+    if (url.length < 10 || linkName.length === 0) return;
     setLinkConnect((prev) => [
       ...prev,
       {
@@ -61,7 +81,12 @@ const MainPages = () => {
       <div className="absolute right-12 lg:right-40 top-[2.5rem]">
         <div className="flex">
           <h1 className="font-bold mr-4 cursor-pointer">목록생성</h1>
-          <h1 className="font-bold mr-4 cursor-pointer">목록삭제</h1>
+          <h1
+            className="font-bold mr-4 cursor-pointer"
+            onClick={handlePageDelete}
+          >
+            목록삭제
+          </h1>
           <h1 className="font-bold mr-4 cursor-pointer">링크연결</h1>
           <Link to={`/note`}>
             <h1 className="font-bold mr-4 cursor-pointer">메모</h1>
@@ -73,7 +98,7 @@ const MainPages = () => {
         <section>
           <ul>
             {mainPages.map((mainPage) => (
-              <Link to={`/${mainPage.pageName}`}>
+              <Link to={`/${mainPage.pageName}`} key={mainPage.id}>
                 <li className="w-[5%] h-10 border list-none">
                   {mainPage.pageName}
                 </li>
@@ -85,7 +110,7 @@ const MainPages = () => {
         <section>
           <ul>
             {linkConnect.map((link) => (
-              <li className="border-yellow-300 border-2">
+              <li className="border-yellow-300 border-2" key={link.id}>
                 <a href={`${link.url}`} className="font-bold">
                   {link.linkName}
                 </a>
@@ -101,6 +126,7 @@ const MainPages = () => {
         </section>
       </main>
 
+      {/* page */}
       <form
         className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-10 border-blue-400 border-4"
         onSubmit={handlepageSubmit}
@@ -113,6 +139,15 @@ const MainPages = () => {
         <button>확인</button>
       </form>
 
+      <form
+        className="absolute top-[50%] left-[60%] border-blue-300 border-4"
+        onSubmit={handlePageDeleteSubmit}
+      >
+        <input value={pageDeleteName} onChange={handlePageDeleteChange} />
+        <button>삭제</button>
+      </form>
+
+      {/* link */}
       <form
         className="absolute top-[50%] left-[50%] translate-x-[-50%] border-green-600 border-2 flex"
         onSubmit={handleCreateLinkSubmit}
